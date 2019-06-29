aeece4a5f6f42ee6bd4f7e3c3fb7d9ed8d310944
Tim Reen <timothy.reen@gmail.com>
2019-06-29 11:31:18 -0400

    check input states

--- src/validate-play/__tests__/index.test.js
+++ src/validate-play/__tests__/index.test.js
@@ -7,4 +7,111 @@ describe("validatePlay", () => {
   it("should be a function", () => {
     expect(typeof validatePlay).toBe("function");
   });
+
+  describe("invalid inputs - should return false", () => {
+    describe("invalid runs scored", () => {
+      it("greater than 4", () => {
+        const res = validatePlay(0, [], 0, [], 5);
+        expect(res).toBeFalsy();
+      });
+
+      it("less than 0", () => {
+        const res = validatePlay(0, [], 0, [], -3);
+        expect(res).toBeFalsy();
+      });
+
+      it("is not a number", () => {
+        const res = validatePlay(0, [], 0, [], "1");
+        expect(res).toBeFalsy();
+      });
+    });
+
+    describe("invalid initialBaserunner", () => {
+      it("not an array", () => {
+        const res = validatePlay(0, "[]", 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values in array are not numbers", () => {
+        const res = validatePlay(0, [1, "3"], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values exist below 1", () => {
+        const res = validatePlay(0, [0, 1], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values exist above 4", () => {
+        const res = validatePlay(0, [1, 3, 4], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("duplicate values exist for a base", () => {
+        const res = validatePlay(0, [1, 1, 3], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+    });
+
+    describe("invalid initialOuts", () => {
+      it("not a number", () => {
+        const res = validatePlay("0", [1, 3], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("less than 0", () => {
+        const res = validatePlay(-1, [1, 3], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("greater than 2", () => {
+        const res = validatePlay(3, [1, 3], 0, [], 1);
+        expect(res).toBeFalsy();
+      });
+    });
+
+    describe("invalid final outs", () => {
+      it("not a number", () => {
+        const res = validatePlay(0, [1, 3], "0", [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("less than 0", () => {
+        const res = validatePlay(0, [1, 3], -1, [], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("greater than 3", () => {
+        const res = validatePlay(0, [1, 3], 4, [], 1);
+        expect(res).toBeFalsy();
+      });
+    });
+
+    describe("invalid finalBaserunners", () => {
+      it("not an array", () => {
+        const res = validatePlay(0, [], 0, "[]", 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values in array are not numbers", () => {
+        const res = validatePlay(0, [], 0, ["1"], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values exist below 1", () => {
+        const res = validatePlay(0, [], 0, [-1], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("values exist above 4", () => {
+        const res = validatePlay(0, [1], 0, [4], 1);
+        expect(res).toBeFalsy();
+      });
+
+      it("duplicate values exist for a base", () => {
+        const res = validatePlay(0, [1], 0, [2, 2], 1);
+        expect(res).toBeFalsy();
+      });
+    });
+  });
 });
--- src/validate-play/index.js
+++ src/validate-play/index.js
@@ -1,5 +1,46 @@
-function validatePlay() {
-  console.log("set up");
-}
+const validatePlay = (
+  initialOuts,
+  initialBaserunners,
+  finalOuts,
+  finalBaserunners,
+  runsScored
+) => {
+  return (
+    validIntialOuts(initialOuts) &&
+    validBaserunners(initialBaserunners) &&
+    validBaserunners(finalBaserunners) &&
+    validFinalOuts(finalOuts) &&
+    validRunsScoredPerPlay(runsScored)
+  );
+};
+
+const validRunsScoredPerPlay = runs => {
+  return typeof runs === "number" && runs >= 0 && runs <= 4;
+};
+
+const validIntialOuts = intitalOuts => {
+  return (
+    typeof intitalOuts === "number" && intitalOuts >= 0 && intitalOuts <= 2
+  );
+};
+
+const validFinalOuts = finalOuts => {
+  return typeof finalOuts === "number" && finalOuts >= 0 && finalOuts <= 3;
+};
+
+const validBaserunners = baserunners => {
+  if (!Array.isArray(baserunners)) return false;
+  if (baserunners.length === 0) return true;
+
+  const valid = baserunners.every(runner => {
+    return typeof runner === "number" && runner >= 1 && runner <= 3;
+  });
+
+  const unique = [...new Set(baserunners)].length === baserunners.length;
+
+  return valid && unique;
+};
+
+// there are 24 possible valid plays -- everything else is invalid
 
 module.exports = validatePlay;
